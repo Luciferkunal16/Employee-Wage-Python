cbeecd75c10fb8beab233e067c062c1823de49c9
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-03-11 11:59:50 +0530

    updated: use dictionary for updating emp hrs

--- src/employeeWage.py
+++ src/employeeWage.py
@@ -15,29 +15,32 @@ class ComputeEmployeeWage:
         self.emp_rate = emp_rate
         self.num_of_days = num_of_days
         self.max_hours = max_hours
+        dict = {}
 
     def calculate_employee_wage_for_company(self):
+        """
+        used to calculate employee wage for company
+        :return:  employee wage per month
+        """
 
         total_emp_hours = 0
         total_working_days = 0
         while total_emp_hours <= self.max_hours and total_working_days < self.num_of_days:
             total_working_days += total_working_days
             emp_check = int(random.randint(0, 2))
-            if emp_check == self.IS_FULL_TIME:
-                emp_hrs = 8
-            elif emp_check == self.IS_PART_TIME:
-                emp_hrs = 4
-            else:
-                emp_hrs = 0
-            total_emp_hours += emp_hrs
-            emp_wage_per_day = emp_hrs * self.emp_rate
+            emp_hrs_dict = {self.IS_FULL_TIME: 8, self.IS_PART_TIME: 4, 0: 0}
+            emp_hrs_dict.get(emp_check)
+            total_emp_hours += emp_hrs_dict.get(emp_check)
+            emp_wage_per_day = emp_hrs_dict.get(emp_check) * self.emp_rate
             self.emp_wage_per_month += emp_wage_per_day
-            print("Employee Wage per Day {}".format(emp_wage_per_day))
 
-        print("Employee Wage Per Month {}".format(self.emp_wage_per_month))
         return self.emp_wage_per_month
 
     def display(self):
+        """
+        used for display computed wage
+        :return:company name
+        """
         print("Company Name= {}".format(self.company))
         print("Employee Wage Per Month {}".format(self.emp_wage_per_month))
         return self.company
@@ -51,10 +54,8 @@ if __name__ == "__main__":
         max_hours = int(input("Enter the Max Hours"))
         company = ComputeEmployeeWage(company_name, emp_rate, num_of_days, max_hours)
         company.calculate_employee_wage_for_company()
-        list_of_company = list()
-        list_of_company.append(company)
-        for item in list_of_company:
-            print(item.display())
+        company.display()
+
 
 
     except Exception as e:
